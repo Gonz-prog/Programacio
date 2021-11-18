51591aad8904f75ce62688655540c043ea05b9b4
Gonzalo Requena Valero <gonsei20@gmail.com>
2021-11-18 12:18:54 +0100

    Update 10.listas.py

--- 10.listas.py
+++ 10.listas.py
@@ -11,21 +11,28 @@
 #"""
 #("C", "R", "A", "M", "T", "C", "h", "m", "s", "o", "y")
 
+# Crear la tupla
 lista1=[]
 t=('C', 'h', 'R', 'm', 'A', 's', 'M', 'o', 'T', 'y', 'C ')
 
+# Convertir en lista
 lista0=list(t)
 
+# Inicio del bucle que comprobará si cada str de la lista0 es .upper()
 for i in range(len(lista0)):
     if lista0[i] == lista0[i].upper():
-
+        
+        # Insertar a la lista los str que indique i
         lista1.append(lista0[i])
 
+# Inicio del bucle que comprobará si cada str de la lista0 es .lower()
 for i in range(len(lista0)):
     if lista0[i] == lista0[i].lower():
-
+        
+        # Insertar a la lista los str que indique i
         lista1.append(lista0[i])
 
+# Pasar a tupla
 res=tuple(lista1)
 
 print(res)
