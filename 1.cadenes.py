ffae05d0e25c5458481340704936773cc84aca08
Gonzalo Requena Valero <gonsei20@gmail.com>
2021-11-18 09:06:31 +0100

    Update 1.cadenes.py

--- 1.cadenes.py
+++ 1.cadenes.py
@@ -1,7 +1,9 @@
 # Programa que muestre por pantalla cada carácter de 
 # una cadena introducida por teclado
 
+#Introducción de datos
 cadena=(input("Introduce una frase: "))
 
+#Recorrer la lista imprimiendo
 for i in cadena:
     print(i)
