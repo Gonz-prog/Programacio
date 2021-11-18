69e84881c1ef545b83acc72c2e86d3eb27cfed45
Gonzalo Requena Valero <gonsei20@gmail.com>
2021-11-18 10:38:42 +0100

    Update 4.cadenas.py

--- 4.cadenas.py
+++ 4.cadenas.py
@@ -13,20 +13,40 @@
 # hasta que se vea cumplido, entonces saltará al esle:
 while True:
 
+    # Introducir str
     email=(input("Introduce un email\n"))
-
+    
+    # Comporbrar si hay una @ en el str, si no
+    # dará error print("Introduce un email valido, xxx@xxx.xxx")
     if "@" not in email or "." not in email:
         print("Introduce un email valido, xxx@xxx.xxx")
+    
+    # Si la anterior condición no se cumple
+    # separaremos el str por la @ indicando 
+    # las dos posiciones del .split("@")[0] .split("@")[1]
     else:
         usuario=email.split("@")[0]
         servidor=email.split("@")[1]
+        
+        # Comprovamos que en estas dos posiciones hay un str
         if len(usuario) == 0 or len(servidor) == 0:
             print("Introduce un email valido, xxx@xxx.xxx")
+        
+        # Si la anterior condición no se cumple
+        # separaremos el str "servidor" por el . indicando 
+        # las dos posiciones del .split(".")[0] .split(".")[1]
         else:
             com1=servidor.split(".")[0]
             com2=servidor.split(".")[1]
+            
+            # Comprovamos que en los dos lados del str "servidor"
+            # haya un str
             if len(com1) == 0 or len(com2) == 0:
                 print("Introduce un email valido, xxx@xxx.xxx")
+            
+            # Si ninguna comprobación da error, entonces 
+            # saldrá del while para imprimir 
+            # la primera separacón que se hizo
             else:
                 break
 print("\nUsuario: ",usuario)    
