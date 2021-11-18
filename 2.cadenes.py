0b37111acd29d643fc47b5e29bbf7cd93e9d2a45
Gonzalo Requena Valero <gonsei20@gmail.com>
2021-11-18 09:17:09 +0100

    Update 2.cadenes.py

--- 2.cadenes.py
+++ 2.cadenes.py
@@ -1,12 +1,17 @@
 # Programa que dada una frase imprima cada palabra en una 
 # línea y nos diga el número de palabras de la frase.
 
+#Itroducir cadena
 cadena="Hacemos Trekking todos los domingos que podemos"
 
+# Separar carácteres por el separador 
+# .split() en este caso un espacio
+# y contar al mismo tiempo las palabras 
 total=len(cadena.split())
 
 cadena=cadena.split()
 
+# Recorrer la lista imprimiendo cada palabra 
 for c in cadena:
     
     print(c)
