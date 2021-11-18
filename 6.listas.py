83ea6a65e950f674fdbbbdd1a946b9a716424f8f
Gonzalo Requena Valero <gonsei20@gmail.com>
2021-11-18 11:36:47 +0100

    Update 6.listas.py

--- 6.listas.py
+++ 6.listas.py
@@ -1,16 +1,14 @@
- # Crear un programa que pida al usuario un número 
- # de mes (por ejemplo, el 5) y diga cuántos días 
- # tiene (por ejemplo, 31) y el nombre del (mayo). 
- # Se deben utilizar listas y supondremos que febrero
- # tiene 28 días.
+# Crear un programa que pida al usuario un número 
+# de mes (por ejemplo, el 5) y diga cuántos días 
+# tiene (por ejemplo, 31) y el nombre del (mayo). 
+# Se deben utilizar listas y supondremos que febrero
+# tiene 28 días.
 
 meses=["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
-dias=[31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+dias=[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-num1=int(input("Dame el numero de un mes: "))
+numero=int(input("Introduce el numero de un mes: "))
 
-if num1 < 13:
-
-    if num1 == 1:
-        print("Mes: ",meses[0])
-        print("Dias: ",dias[0])
+for i in range(numero):
+    a=1
+print("\n",meses[i],dias[i],"\n")
