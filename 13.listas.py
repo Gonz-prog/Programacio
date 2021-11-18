624e034719cea49618670bfb433aa2a4561bebca
Gonzalo Requena Valero <gonsei20@gmail.com>
2021-11-18 12:36:42 +0100

    Update 13.listas.py

--- 13.listas.py
+++ 13.listas.py
@@ -10,20 +10,28 @@
 # la cereza está presente 2 veces
 # la fresa está presente 1 vez
 
+# Introducir lista
 añadir0=[]
 
+# Cadena
 cadena="manzana|pera|manzana|cereza|pera|manzana|pera|pera|cereza|pera|fresa"
 
+# Crear la lista palabras a partir de la cadena quitando el separador con .split("|")
 palabras=list(cadena.split("|"))
 
+# Inicio del bucle que añadirá i más el número de carácteres en la lista contar 
 for i in palabras:
     contar=[]
     contar.append(i)
     contar.append(palabras.count(i))
-
+    
+    # Inicio del bucle en el rango de la lista palabras 
     for i in range(len(palabras)):
+        
+        # Pasar a tupla
         contar=tuple(contar)
     añadir0.append(contar)
+    añadir=tuple(añadir0)
     añadir1=list(añadir0)
 
 print("\ncuenta =",añadir1,"\n")
