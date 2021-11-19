644830a42af1d93604256ebb0108433d34062b47
Gonzalo Requena Valero <gonsei20@gmail.com>
2021-11-19 16:40:58 +0100

    Update 17.listas.py

--- 17.listas.py
+++ 17.listas.py
@@ -1,1 +1,22 @@
+# •Actualizar conjunto1 añadiendo elementos del conjunto2, 
+# excepto los elementos que son iguales.
 
+conjunto1 = {10, 20, 30, 40, 50} 
+conjunto2 = {30, 40, 50, 60, 70}
+
+s1=list(conjunto1)
+s2=list(conjunto2)
+
+lista=[]
+
+for i in s2:
+    if i not in s1:
+        lista.append(i)
+s1.append(lista)
+
+
+
+print("\nConjunto actualizado",s1,"\n")
+
+# •Eliminar elementos de conjunto1 que no estén en el 
+# 1ºy el 2ºconjunto.
