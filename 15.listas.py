45bfbebc3c3d864537ec59582ad582a017c51460
Gonzalo Requena Valero <gonsei20@gmail.com>
2021-11-19 14:14:07 +0100

    Update 15.listas.py

--- 15.listas.py
+++ 15.listas.py
@@ -2,36 +2,70 @@
 # conjunto1 = {10, 20, 30, 40, 50}
 # conjunto2 = {30, 40, 50, 60, 70}
 
-# •1: Devolver un conjunto de los elementos idénticos 
-# en los dos conjuntos.
-
-# •2: Vuelve un conjunto nuevo con todos los elementos 
-# de los dos conjuntos eliminando los duplicados.
-
-# •3: Actualizar el primer conjunto con elementos 
-# que sólo existen en el primer conjunto y no en el
-# segundo conjunto.
-
-# •4: Sacar los elementos 30, 40 del primer conjunto.
-
-# •5: Devolver un conjunto de todos los elementos de 
-# conjunt1 o conjunt2, pero no de ambos conjuntos.
-
 conjunto1 = {10, 20, 30, 40, 50}
 conjunto2 = {30, 40, 50, 60, 70}
 
-# 1
+# 1 Devolver un conjunto de los elementos idénticos 
+# en los dos conjuntos.
+uno3=[]
+uno1=list(conjunto1)
+uno2=list(conjunto2)
+for i in uno2:
+    if i in uno1:
+        uno3.append(i)
+
+uno4=set(uno3)
+
+print("\n1:",uno4,"\n")
+
+# 2 Vuelve un conjunto nuevo con todos los elementos 
+# de los dos conjuntos eliminando los duplicados.
+
+dos1=list(conjunto1)
+dos2=list(conjunto2)
+
+for i in dos2:
+    if i not in dos1:
+        dos1.append(i)
+
+dos1.sort()
+dos3=set(dos1)
+
+print("2:",dos3,"\n")
+
+# 3 Actualizar el primer conjunto con elementos 
+# que sólo existen en el primer conjunto y no en el
+# segundo conjunto.
+
 c1=set(conjunto1)
 c1.difference_update(conjunto2)
 
-print("\n1:",c1,"\n")
+print("3:",c1,"\n")
 
-# 2
-conjunto1.update(conjunto2)
-conjunto1.discard(conjunto1)
+# 4 Sacar los elementos 30, 40 del primer conjunto.
 
-print("\n2:",conjunto1,"\n")
+cuat1=list(conjunto1)
 
-# 3
+cuat1.pop(2)
+cuat1.pop(3)
 
-print("\n3:",conjunto1,"\n")
+cuat2=set(cuat1)
+
+print("4:",cuat2,"\n")
+
+# 5 Devolver un conjunto de todos los elementos de 
+# conjunt1 o conjunt2, pero no de ambos conjuntos.
+
+cinc1=set(conjunto1)
+cinc2=set(conjunto2)
+cinc3=[]    
+for i in range(len(cinc1)):
+    x=cinc1.pop()
+    y=cinc2.pop()
+    cinc3.append(x)
+    cinc3.append(y)
+    
+
+cinc4=set(cinc3)
+
+print("5:",cinc4,"\n")
