ccd2b4753d82942f2abe43833260fb4ece9427e6
Gonzalo Requena Valero <gonsei20@gmail.com>
2021-11-25 14:01:24 +0100

    Update funcionesX.py

--- funcionesX.py
+++ funcionesX.py
@@ -11,3 +11,9 @@ print("\n",funciones.areaRectangle(3,4))
 valors=[1, 4, 7, 8]
 
 print("\n",funciones.media(valors))
+
+print(
+    40*("-")\
+    ,"\n",funciones.media(valors),"\n"\
+    +40*("-")    
+)
