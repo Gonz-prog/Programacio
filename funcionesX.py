53101ae45e14b4382dfd40fcc3b1901e3d7274cc
Gonzalo Requena Valero <gonsei20@gmail.com>
2021-11-25 13:54:41 +0100

    Create funcionesX.py

---
+++ funcionesX.py
@@ -0,0 +1,13 @@
+import funciones
+
+print("\n",funciones.sumar(4,5))
+
+x=funciones.listar("Gonzalo")
+
+print("\n",x)
+
+print("\n",funciones.areaRectangle(3,4))
+
+valors=[1, 4, 7, 8]
+
+print("\n",funciones.media(valors))
